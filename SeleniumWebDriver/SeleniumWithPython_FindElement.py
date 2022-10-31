17fa8383162f6a11dda17284d029e23e4e1baa69
shidurbaba <shidurbaba@gmail.com>
2022-10-31 08:46:24 -0400

    updated

--- SeleniumWebDriver/SeleniumWithPython_FindElement.py
+++ SeleniumWebDriver/SeleniumWithPython_FindElement.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,4 +10,19 @@ driver = webdriver.Chrome(service=service_object)
 driver.maximize_window()
 driver.implicitly_wait(9)
 driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
 # WebElements in CSS
+driver.find_element(By.CSS_SELECTOR, "#autosuggest").send_keys("can")
+time.sleep(3)
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+print(len(countries))
+
+#for loop condition to select dyanic country
+for country in countries:
+    if country.text == "Canada":
+        country.click()
+        break
+
+time.sleep(3)
+
+driver.close()
