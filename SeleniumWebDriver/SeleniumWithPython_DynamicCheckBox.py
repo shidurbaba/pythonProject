b9593415e41bfe3b8142dde289c47c8fb5c2bce9
shidurbaba <shidurbaba@gmail.com>
2022-10-31 14:07:04 -0400

    updated 10/31/2022

--- SeleniumWebDriver/SeleniumWithPython_DynamicCheckBox.py
+++ SeleniumWebDriver/SeleniumWithPython_DynamicCheckBox.py
@@ -9,19 +9,35 @@ service_object = Service("C:\chromedriver.exe")
 driver = webdriver.Chrome(service=service_object)
 driver.maximize_window()
 driver.implicitly_wait(9)
-driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
 # WebElements in CSS
-driver.find_element(By.CSS_SELECTOR, "#autosuggest").send_keys("can")
-time.sleep(3)
-countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
-print(len(countries))
+radio_buttons = driver.find_elements(By.CSS_SELECTOR, "#radio-btn-example input")
+print(len(radio_buttons))
 
-#for loop condition to select dyanic country
-for country in countries:
-    if country.text == "Canada":
-        country.click()
-        break
+time.sleep(3)
+for radio_button in radio_buttons:
+    if radio_button.get_attribute("value") == "radio1":
+        radio_button.click()
+        assert radio_button.is_selected()
+
+element_display = driver.find_element(By.CSS_SELECTOR, "#displayed-text")
+hide_button = driver.find_element(By.CSS_SELECTOR, "#hide-textbox")
+
+assert element_display.is_displayed()
+hide_button.click()
+time.sleep(3)
+
+#Alert Message
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys("Python_Automator")
+driver.find_element(By.CSS_SELECTOR, "#confirmbtn").click()
+
+time.sleep(3)
+
+alert = driver.switch_to.alert
+print(alert.text)
+assert "Python_Automator" in alert.text
+alert.accept()
 
 time.sleep(3)
 
