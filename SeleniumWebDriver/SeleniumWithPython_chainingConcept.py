d839ea4d40846d604637bbf59a0038488667c54d
shidurbaba <shidurbaba@gmail.com>
2022-11-22 14:29:57 -0500

    updated 11/22/2022 chaining concept

--- SeleniumWebDriver/SeleniumWithPython_chainingConcept.py
+++ SeleniumWebDriver/SeleniumWithPython_chainingConcept.py
@@ -31,4 +31,36 @@ for result in results:
     result.find_element(By.XPATH, "div/button").click()
 
 print(actual_list)
+time.sleep(3)
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+# time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "div[class='cart-preview active'] button").click()
+
+#Sum my total algo
+prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+print(sum)
+
+#total amount displayed on the screen after login
+total_amount_displayed = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+assert sum == total_amount_displayed
+
+driver.find_element(By.CSS_SELECTOR, "input[placeholder='Enter promo code']").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+#Explicit Wait Conditions:
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
+
+promo_message=driver.find_element(By.CSS_SELECTOR,".promoInfo").text
+print(promo_message)
+
+#total amount should less than discount amount
+discount_amount_displayed = driver.find_element(By.CSS_SELECTOR, ".discountAmt").text
+print(discount_amount_displayed)
+assert int(float(discount_amount_displayed)) < int(total_amount_displayed)
+# assert int(discount_amount_displayed) < int(total_amount_displayed)
 driver.close()
