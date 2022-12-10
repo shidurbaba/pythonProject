d07158445f4ad699a048de7d27fcf2770f587cea
shidurbaba <shidurbaba@gmail.com>
2022-12-09 20:12:16 -0500

    updated 12/09/2022

---
+++ SeleniumWebDriver/SeleniumWithPython_HeadlessMode.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+#Headless Mode
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("--ignore-certificate-error")
+
+service_object = Service("C:\chromedriver.exe")
+
+driver = webdriver.Chrome(service=service_object, options=chrome_options)
+driver.maximize_window()
+driver.implicitly_wait(9)
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+#Scroll to the bottom of the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#driver.get_screenshot_as_file("headlessScreenShot.png")
+
+print("execution_successful")
